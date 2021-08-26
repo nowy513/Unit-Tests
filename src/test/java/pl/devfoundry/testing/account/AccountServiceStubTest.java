7426660dfc7175f05844c4ceea24e1c705181032
Slawek Swietoniowski <slawomir.swie@gmail.com>
2021-08-26 13:34:17 +0200

    Mocki

---
+++ src/test/java/pl/devfoundry/testing/account/AccountServiceStubTest.java
@@ -0,0 +1,26 @@
+package pl.devfoundry.testing.account;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceStubTest {
+
+    @Test
+    void getAllActiveAccounts() {
+
+//        Given
+        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
+        AccountService accountService = new AccountService(accountRepositoryStub);
+
+//        When
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+//        Then
+        assertThat(accountList, hasSize(2));
+    }
+}
